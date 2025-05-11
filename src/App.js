99a76202ddb33b9b964c5975fb884eb62877efb1
HARSH SISODIA <rajputh322@gmail.com>
2025-05-11 20:27:31 +0530

    add & update files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import textForm from './Components/textForm';
 
 function App() {
   return (
@@ -8,9 +9,11 @@ function App() {
   //using fragment tag to avoid using div tag
   //and to avoid using extra div tag in the DOM
 <>
-{/* <Navbar title = "TextUtils" aboutText="About"/> */}
+<Navbar title = "TextUtils" aboutText="About"/>
+<textForm/>
+{/* <Navbar/> */}
 {/* <Navbar title = "TextUtils"/> */}
-<Navbar/>
+
  </>
   );
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 export default function Navbar(props) {
-      console.log("About Text:", props.aboutText);
+      // console.log("AboutText:", props.aboutText);
+      // console.log("App rendered");
+      // console.log("Navbar props:", props);
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="click">{props.title}</a>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -26,6 +28,7 @@ export default function Navbar(props) {
   </div>
 </nav>
     )
+  
 }
 Navbar.propTypes={
             title:  PropTypes.string,
